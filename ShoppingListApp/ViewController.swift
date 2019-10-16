bd0d6f36c99a06a952bb92465d0384611177b74e
Angadjot Singh <angadjyot95@gmail.com>
2019-10-16 19:28:37 -0400

    first implemented with tableview,textfield,add cancel and save button

--- ShoppingListApp/ViewController.swift
+++ ShoppingListApp/ViewController.swift
@@ -8,13 +8,59 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource { 
 
+    
+    
+    @IBOutlet weak var textfieldItem: UITextField!
+    @IBOutlet weak var addButton: UIButton!
+    @IBOutlet weak var saveButton: UIButton!
+    @IBOutlet weak var cancelButton: UIButton!
+    
+    
+    var shoppingListArray = [String]()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        addButton.layer.cornerRadius = 10.0
+        addButton.layer.masksToBounds = true
+        
+        saveButton.layer.cornerRadius = 10.0
+        saveButton.layer.masksToBounds = true
+        
+        cancelButton.layer.cornerRadius = 10.0
+        cancelButton.layer.masksToBounds = true
+        
     }
 
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return shoppingListArray.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell:UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "cell")!
+        
+        cell.textLabel?.text = "hello"
+        return cell
+        
+    }
+    
+    @IBAction func addButtonAction(_ sender: UIButton) {
+        let item = textfieldItem.text
+        self.shoppingListArray.append(item!)
+        print("array is",shoppingListArray)
+    }
+    
+    
 
 }
 
