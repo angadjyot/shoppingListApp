b862505942d36b203655610c729cb8d1db58d22d
Angadjot Singh <angadjyot95@gmail.com>
2019-10-16 19:03:26 -0400

    project started and splash screen implemented

---
+++ ShoppingListApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ShoppingListApp
+//
+//  Created by Angadjot singh on 16/10/19.
+//  Copyright © 2019 Angadjot singh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
