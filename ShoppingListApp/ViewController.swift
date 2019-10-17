2577fdcaa4f94eff670405afd96dcffc5790deab
Angadjot Singh <angadjyot95@gmail.com>
2019-10-16 21:00:05 -0400

    cancel button clears out everything

--- ShoppingListApp/ViewController.swift
+++ ShoppingListApp/ViewController.swift
@@ -22,7 +22,18 @@ class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource
     
     
     var shoppingListArray = [String]()
+    var quantity = 1.0
     
+    var arrayCount = 0
+    
+    @IBAction func wssw(_ sender: UIStepper) {
+        
+        print("stepper working")
+        print(sender.value)
+        quantity = sender.value
+        print("quantity is",quantity)
+        self.shoppinglistTableView.reloadData()
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -49,21 +60,49 @@ class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return shoppingListArray.count
     }
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell:UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "cell")!
         
         if let itemName = cell.viewWithTag(1) as? UILabel{
             itemName.text = shoppingListArray[indexPath.row]
         }
+    
+        if let quantityLabel = cell.viewWithTag(3) as? UILabel{
+            
+             quantityLabel.text = String(quantity)
+            
+        }
+        
+        
+//        if let quantityLabel = cell.viewWithTag(3) as? UILabel{
+//
+//            if arrayCount == 1{
+//                if indexPath.row == 0{
+//                    quantityLabel.text = String(quantity)
+//                }
+//            }
+//
+//           else if arrayCount == 2{
+//                if indexPath.row == 1{
+//                    quantityLabel.text = String(quantity)
+//                }
+//            }
+//        }
+    
         return cell
         
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("indexPath.row",indexPath.row)
         
         
         
-        
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 75
     }
     
     
@@ -71,8 +110,27 @@ class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource
         let item = textfieldItem.text
         self.shoppingListArray.append(item!)
         print("array is",shoppingListArray)
+        
+        arrayCount = shoppingListArray.count
+        print("count is",shoppingListArray.count)
+        
         self.shoppinglistTableView.reloadData()
     }
+    
+    
+    @IBAction func save(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func cancel(_ sender: UIButton) {
+        self.shoppingListArray.removeAll()
+        self.quantity = 1.0
+        self.shoppinglistTableView.reloadData()
+    }
+    
+    
+    
+    
     
     
 
